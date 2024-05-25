2b59aca0d5f4e7c5c9bb8a53a51342d86983fabd
user <credomixxxx@gmail.com>
2024-05-25 11:39:34 +0300

    Lab 5&6

---
+++ XmlTransformer/XsltSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+using System.Xml.Xsl;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace XmlTransformer
+{
+    public static class XmlTransformer
+    {
+        public static void SerializeWithXslt(string xmlPath, string xsltPath, string outputPath)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlPath);
+
+            XslCompiledTransform transform = new XslCompiledTransform();
+
+            transform.Load(xsltPath);
+            using (TextWriter writer = new StreamWriter(outputPath))
+            {
+                transform.Transform(doc, null, writer);
+            }
+        }
+    }
+}
